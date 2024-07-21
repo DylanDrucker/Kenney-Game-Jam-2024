7d55ac7bdf821352c8356805cd26d0ff5da26224
Dylan Drucker <s2077148@ed.ac.uk>
2024-07-21 15:33:21 +0100

    warning icon

--- Scenes/warning_message.gd
+++ Scenes/warning_message.gd
@@ -8,6 +8,6 @@ func show_message(warning_text):
 	text = "Warning: " + warning_text
 	for i in range(3):
 		visible = true
-		await get_tree().create_timer(0.5).timeout
+		await get_tree().create_timer(0.8).timeout
 		visible = false
-		await get_tree().create_timer(0.5).timeout
+		await get_tree().create_timer(0.8).timeout
